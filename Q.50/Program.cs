7826d5af90a6ddd0899f9efc125ec626e0352c93
Zohaib-Hussain <81561676+Zohaib-Hussain@users.noreply.github.com>
2021-06-10 16:18:39 +0500

    Add files via upload

---
+++ Q.50/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Q._50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             int x = 4;
+             Console.WriteLine(x*x +3*x -4);
+             Console.Read(); */
+
+            /*
+            int x = 2, y = 3, z = 4;
+            Console.WriteLine((x + y)*z);
+            Console.Read(); */
+
+
+            /*
+            int x = 2;
+            Console.WriteLine(1 / (x * x + x + 3));
+            Console.Read();  */
+
+            /*
+            int x = 4, y = 2, z = 1;
+            Console.WriteLine((x = (3 * y) / (5 - z)));
+            Console.Read(); */
+
+            /*
+            int z = 4;
+            Console.WriteLine((z = area * sqrt (area)));
+            Console.Read(); */
+
+            /*
+            int res;
+            int i = 3, j = 2, k = 1;
+            Console.WriteLine(3 * i * j * k + k ^ 9) / (7 * i * k + 5 * sqrt(j + k)); */
+
+            /*
+            int b = 2, c = 3;
+            Console.WriteLine(2 * b * c * c * c);
+            Console.Read(); */
+
+
+
+
+
+
+
+        }
+    }
+}
