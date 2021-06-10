7b688fea62edd8923d26c2b307f524541111ece3
Zohaib-Hussain <81561676+Zohaib-Hussain@users.noreply.github.com>
2021-06-10 16:21:01 +0500

    Add files via upload

---
+++ Q.55/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Q._55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int a = 3, b = 4;
+            ++a * = b++; 
+            Console.WriteLine(a);
+            Console.Read(); */
+
+            /*
+            int a = 3, b = 4;
+            a++; a *= ++b;
+            Console.WriteLine(a);
+            Console.Read(); */
+
+            /*
+            int a = 3, b = 4;
+            a = a + 1;  b = b + 1;
+            b = b + 1; a = a + 1;
+            a = a * b; a = a * b;
+            Console.WriteLine(a);
+            Console.Read(); */
+
+        }
+    }
+}
