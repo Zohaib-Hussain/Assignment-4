391202efc9e95737baa32a0338f28cf3765a48ec
Zohaib-Hussain <81561676+Zohaib-Hussain@users.noreply.github.com>
2021-06-10 16:21:38 +0500

    Add files via upload

---
+++ Q.58/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Q._58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int a = 2, b = 3, c = 5;
+            Console.WriteLine(++a);
+            Console.Read();
+            Valid: 3,3,5 */
+
+            /*
+            int a = 2, b = 3, c = 5;
+            Console.WriteLine(abc);
+            Console.Read();
+            Invalid */
+
+            /*
+            int a = 2, b = 3, c = 5;
+            Console.WriteLine(a++);
+            Console.Read();
+            Valid 2,2,5 */
+
+            /*
+            int a = 2, b = 3, c = 5;
+            Console.WriteLine(b += a);
+            Console.Read();
+            Valid 2,5,5 */
+
+            /*
+            int a = 2, b = 3, c = 5;
+            Console.WriteLine(b += a * c);
+            Console.Read();
+            Valid 2,13,5 */
+
+            /*
+            int a = 2, b = 3, c = 5;
+            Console.WriteLine(a++b);
+            Console.Read();
+            Invalid */
+
+            /*
+            int a = 2, b = 3, c = 5;
+            Console.WriteLine(a + = 2; b = 2 / 2; c = a * b;);
+            Console.Read();
+            Valid 4,1,4 */
+
+            /*
+            int a = 2, b = 3, c = 5;
+            Console.WriteLine(++b++);
+            Console.Read();
+            Invalid */
+
+            /*
+            int a = 2, b = 3, c = 5;
+            Console.WriteLine(b = a++ + b++);
+            Console.Read();
+            Valid 3,6,5 */
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
