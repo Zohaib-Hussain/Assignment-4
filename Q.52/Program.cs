8f2cac609a0c9594b20d379bc9e7c4b50b029b6a
Zohaib-Hussain <81561676+Zohaib-Hussain@users.noreply.github.com>
2021-06-10 16:19:30 +0500

    Add files via upload

---
+++ Q.52/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Q._50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           /* 
+            Console.WriteLine((1 + 1 + 1 + 1) / 2 + (1 + 1 + 1) / 3); 
+            Console.Read(); */
+
+             
+            /* 
+             Console.WriteLine(1 + 8 / 2 + ((1 * 4) + (5 * 4)) / 4);
+            Console.Read(); */
+
+            /* 
+             Console.WriteLine(5 * 5 + 5 / 5 + 6);
+            Console.Read();*/
+
+            /* 
+             Console.WriteLine(((3 + 4) + (4 * 7)) / 5);
+            Console.Read();*/
+
+            /*
+             Console.WriteLine((3 * 6 * 7 * 2) + 12 / 2);
+            Console.Read();*/
+
+            /* 
+             Console.WriteLine(5 - 3 * 4 % (6 - 1));
+            Console.Read();*/
+
+            /* 
+             Console.WriteLine((8 * 4 * 2 + 6) / 2 + 4);
+            Console.Read();*/
+
+
+
+
+
+                            
+                             
+       }
+    }
+}
