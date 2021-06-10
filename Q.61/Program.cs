20bf492cb3e0d4bd89df6b733fe0595f4b663c04
Zohaib-Hussain <81561676+Zohaib-Hussain@users.noreply.github.com>
2021-06-10 16:22:52 +0500

    Add files via upload

---
+++ Q.61/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Q._61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int a = 10, b = 20, c = 15, d = 8, e = 40;
+            Console.WriteLine((a+b/(c-5))/((d+7)/(e-37)%3));
+            Console.Read(); */
+
+            /*
+            int a = 10, b = 20, c = 15, d = 8, e = 40;
+            Console.WriteLine(a + b / c - 5 / d + 7 / e - 37 % 3);
+            Console.Read(); */
+
+            /*
+            int a = 10, b = 20, c = 15, d = 8, e = 40;
+            Console.WriteLine(a * (b * b) - (c * b) + d);
+            Console.Read(); */
+
+
+        }
+    }
+}
