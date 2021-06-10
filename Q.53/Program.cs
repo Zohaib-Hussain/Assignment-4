64522664afa3ff5335957b7b4c2f0fc6238f5c2c
Zohaib-Hussain <81561676+Zohaib-Hussain@users.noreply.github.com>
2021-06-10 16:20:16 +0500

    Add files via upload

---
+++ Q.53/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Q._53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             int x = 20,  y = 35;
+            x = y++ + x++;
+            y = ++y + ++x;
+            Console.WriteLine(x + " " + y);
+            Console.Read(); */
+
+            /*
+             int x = 10, y = 15;
+            x = x++;
+            y = ++y;
+            Console.WriteLine(x + " " + y);
+            Console.Read(); */
+
+            /*
+            int a = 10;
+            a++;
+            Console.WriteLine(a);
+            Console.Read(); */
+
+            /*
+            int a = 10;
+            Console.WriteLine(a++);
+            Console.Read(); */
+
+            /*
+            int n = 10;
+            int x = 0;
+            x = n--;
+            Console.WriteLine(x);
+            Console.Read(); */
+
+            /*
+            int i = 5, j = 6, k = 7, n = 3;
+            Console.WriteLine(i + j * k - k % n);
+            Console.WriteLine(i / n);
+            Console.Read(); */
+
+            /*
+            int n;
+            float x = 3.8F;
+            n= ((int)x);
+            Console.WriteLine(n = " " + n);
+            Console.Read(); */
+
+            /*
+            int a = 6;
+            Console.Write(a);
+            a = a + 3;
+            Console.Write(a);
+            a -= 5;
+            Console.Write(a);
+            a++;
+            Console.Write(a);
+            Console.Read(); */
+
+            
+            
+
+
+        }
+    }
+}
